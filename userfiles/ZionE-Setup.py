a88438410cf20d08214c0aa4692b73ce28c5ff02
Bob Jacobsen <jacobsen@mac.com>
2024-02-15 12:48:46 -0500

    add some delays

--- userfiles/ZionE-Setup.py
+++ userfiles/ZionE-Setup.py
@@ -4,60 +4,88 @@
 # Organized from right to left
 # 
 
-# Remote levers
-sensors.getSensor("ZE XY Remote - Local").state = ACTIVE
-sensors.getSensor("ZE Remote - Local").state = INACTIVE
+import jmri
 
-# Whiskey occupancy and signals
-sensors.getSensor("Whiskey 1 occupancy").state = INACTIVE
-sensors.getSensor("Whiskey 2 occupancy").state = INACTIVE
-sensors.getSensor("Whiskey 3 occupancy").state = INACTIVE
-signals.getSignalHead("Whiskey 1 Exit W").appearance = GREEN
-signals.getSignalHead("Whiskey 2 Exit W").appearance = GREEN
-signals.getSignalHead("Whiskey 3 Exit W").appearance = GREEN
+class InitializeZionEPanel(jmri.jmrit.automat.AbstractAutomaton) :
 
-sensors.getSensor("Whisky Xerox transition occupancy").state = INACTIVE
-sensors.getSensor("Xerox 1 occupancy").state = INACTIVE
-sensors.getSensor("Xerox 2 occupancy").state = INACTIVE
+    # handle() is called repeatedly until it returns false.
+    def handle(self):
 
-# Yazoo NX buttons
-turnouts.getTurnout("ZE NX X1 request").state = CLOSED
-turnouts.getTurnout("ZE NX X2 request").state = CLOSED
-turnouts.getTurnout("ZE NX Y1 request").state = CLOSED
-turnouts.getTurnout("ZE NX Y2 request").state = CLOSED
-turnouts.getTurnout("ZE NX Y3 request").state = CLOSED
-sensors.getSensor("ZE NX X1 ack").state = INACTIVE
-sensors.getSensor("ZE NX X2 ack").state = INACTIVE
-sensors.getSensor("ZE NX Y1 ack").state = INACTIVE
-sensors.getSensor("ZE NX Y2 ack").state = INACTIVE
-sensors.getSensor("ZE NX Y3 ack").state = INACTIVE
+        # Remote levers
+        sensors.getSensor("ZE XY Remote - Local").state = ACTIVE
+        sensors.getSensor("ZE Remote - Local").state = INACTIVE
 
-# Zion team tracks power
-sensors.getSensor("ZE Team power ack").state = ACTIVE
-sensors.getSensor("ZE Team power request").state = INACTIVE
+        # Whiskey occupancy and signals - defer to gateway
+        #sensors.getSensor("Whiskey 1 occupancy").state = INACTIVE
+        #sensors.getSensor("Whiskey 2 occupancy").state = INACTIVE
+        #sensors.getSensor("Whiskey 3 occupancy").state = INACTIVE
+        #signals.getSignalHead("Whiskey 1 Exit W").appearance = GREEN
+        #signals.getSignalHead("Whiskey 2 Exit W").appearance = GREEN
+        #signals.getSignalHead("Whiskey 3 Exit W").appearance = GREEN
 
-# Zion power buttons are single-address Sensors
-sensors.getSensor("ZE F1 power request").state = INACTIVE
-sensors.getSensor("ZE F2 power request").state = INACTIVE
-sensors.getSensor("ZE F3 power request").state = INACTIVE
-sensors.getSensor("ZE F4 power request").state = INACTIVE
-sensors.getSensor("ZE F5 power request").state = INACTIVE
-sensors.getSensor("ZE F6 power request").state = INACTIVE
-sensors.getSensor("ZE F7 power request").state = INACTIVE
-sensors.getSensor("ZE F8 power request").state = INACTIVE
+        #sensors.getSensor("Whisky Xerox transition occupancy").state = INACTIVE
+        #sensors.getSensor("Xerox 1 occupancy").state = INACTIVE
+        #sensors.getSensor("Xerox 2 occupancy").state = INACTIVE
 
-sensors.getSensor("ZE P1 power request").state = INACTIVE
-sensors.getSensor("ZE P2 power request").state = INACTIVE
-sensors.getSensor("ZE P3 power request").state = INACTIVE
-sensors.getSensor("ZE P4 power request").state = INACTIVE
-sensors.getSensor("ZE P5 power request").state = INACTIVE
-sensors.getSensor("ZE P6 power request").state = INACTIVE
-sensors.getSensor("ZE P7 power request").state = INACTIVE
+        # Yazoo NX buttons
+        # turnouts.getTurnout("ZE NX X1 request").state = CLOSED
+        # turnouts.getTurnout("ZE NX X2 request").state = CLOSED
+        # turnouts.getTurnout("ZE NX Y1 request").state = CLOSED
+        # turnouts.getTurnout("ZE NX Y2 request").state = CLOSED
+        # turnouts.getTurnout("ZE NX Y3 request").state = CLOSED
+        sensors.getSensor("ZE NX X1 ack").state = INACTIVE
+        self.waitMsec(50)
+        sensors.getSensor("ZE NX X2 ack").state = INACTIVE
+        self.waitMsec(50)
+        sensors.getSensor("ZE NX Y1 ack").state = INACTIVE
+        self.waitMsec(50)
+        sensors.getSensor("ZE NX Y2 ack").state = INACTIVE
+        self.waitMsec(50)
+        sensors.getSensor("ZE NX Y3 ack").state = INACTIVE
+        self.waitMsec(50)
 
-sensors.getSensor("ZE Misc power request").state = INACTIVE
+        # Set ladders to a particular track
+        turnouts.getTurnout("ZE EABECR FrtA request").state = THROWN
+        turnouts.getTurnout("ZE EABECR FrtB request").state = THROWN
+        turnouts.getTurnout("ZE T1 request").state = THROWN
+        turnouts.getTurnout("ZE P1 request").state = THROWN
+        turnouts.getTurnout("ZE F1 request").state = THROWN
+        self.waitMsec(50)
+        turnouts.getTurnout("ZE F8 request").state = THROWN  # clear Run West
+        self.waitMsec(50)
+        turnouts.getTurnout("ZE F5 request").state = THROWN
 
+        # Zion team tracks power
+        sensors.getSensor("ZE Team power ack").state = ACTIVE
+        sensors.getSensor("ZE Team power request").state = INACTIVE
 
+        # Zion power buttons are single-address Sensors
+        sensors.getSensor("ZE F1 power request").state = INACTIVE
+        sensors.getSensor("ZE F2 power request").state = INACTIVE
+        sensors.getSensor("ZE F3 power request").state = INACTIVE
+        sensors.getSensor("ZE F4 power request").state = INACTIVE
+        sensors.getSensor("ZE F5 power request").state = INACTIVE
+        sensors.getSensor("ZE F6 power request").state = INACTIVE
+        sensors.getSensor("ZE F7 power request").state = INACTIVE
+        sensors.getSensor("ZE F8 power request").state = INACTIVE
 
-#sensors.getSensor("").state = INACTIVE
-#turnouts.getTurnout("").state = CLOSED
-#signals.getSignalHead("").appearance = GREEN
+        sensors.getSensor("ZE P1 power request").state = INACTIVE
+        sensors.getSensor("ZE P2 power request").state = INACTIVE
+        sensors.getSensor("ZE P3 power request").state = INACTIVE
+        sensors.getSensor("ZE P4 power request").state = INACTIVE
+        sensors.getSensor("ZE P5 power request").state = INACTIVE
+        sensors.getSensor("ZE P6 power request").state = INACTIVE
+        sensors.getSensor("ZE P7 power request").state = INACTIVE
+
+        sensors.getSensor("ZE Misc power request").state = INACTIVE
+
+        return False
+
+# create one of these
+a = InitializeZionEPanel()
+
+# set the name, as a example of configuring it
+a.setName("Initialize Zion E Panel")
+
+# and start it running - this will only take a short time
+a.start()
