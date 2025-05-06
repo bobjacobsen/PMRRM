d09e4a262067187c645c4cb10617de38f6f3c599
Bob Jacobsen <jacobsen@mac.com>
2025-05-06 07:20:05 -0400

    re-enable Xerox E turnout

--- Profiles/Dispatcher_Simple.jmri/ConvertComplexToSimple.py
+++ Profiles/Dispatcher_Simple.jmri/ConvertComplexToSimple.py
@@ -1,5 +1,5 @@
-# a script for the PMRRM to suppress (make invisible and inactive) the 
-# "yellow dot" sensor icons used for NX routing.
+# a script for the PMRRM to convert the Dispatcher Complex profile into
+# the desired Dispatcher Simple configuration
 #
 # Bob Jacobsen    2025
 
@@ -9,7 +9,13 @@ import jmri
 panelMenu = jmri.InstanceManager.getDefault(jmri.jmrit.display.EditorManager)
 layoutPanels = panelMenu.getList(jmri.jmrit.display.layoutEditor.LayoutEditor)
 for layoutEditor in layoutPanels :
+    # disable and hide the "yellow dot" sensor icons
     for p in layoutEditor.getSensorList() :
         if p.getSensor().getUserName().startswith("NX") :
             p.setVisible(False)
             p.setControlling(False)
+
+    # re-enable the Xerox E turnout icon
+    for p in layoutEditor.getLayoutTurnouts() :
+        if p.getTurnoutName() == "Xerox E" :
+            p.setDisabled(False)
